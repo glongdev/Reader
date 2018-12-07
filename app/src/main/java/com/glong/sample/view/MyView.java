502dd843652f8392128041498231e05546f636bd
glong <krouky@outlook.com>
2018-12-07 20:05:50 +0800

    完善Demo

---
+++ app/src/main/java/com/glong/sample/view/MyView.java
@@ -0,0 +1,10 @@
+package com.glong.sample.view;
+
+import android.view.View;
+
+/**
+ * Created by Garrett on 2018/12/7.
+ * contact me krouky@outlook.com
+ */
+public class MyView extends View {
+}
